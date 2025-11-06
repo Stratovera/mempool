38dbf9456eae51096146029f2f71cbc918a768d9
rc-stratovera <robert@stratovera.io>
2025-11-05 22:40:38 -0800

    fix: persist rpc credentials across redeploys

--- lib/bitcoind.sh
+++ lib/bitcoind.sh
@@ -22,6 +22,35 @@ get_rpc_password() {
     eval "echo \${RPC_PASS_${network}:-}"
 }
 
+rpc_credentials_file() {
+    local network="$1"
+    echo "${MEMPOOL_BASE_DIR}/${network}/.rpc_credentials"
+}
+
+load_rpc_credentials() {
+    local network="$1"
+    local file
+    file="$(rpc_credentials_file "$network")"
+    [[ -f "$file" ]] || return 1
+    # shellcheck disable=SC1090
+    source "$file"
+    [[ -n "${RPC_USER:-}" && -n "${RPC_PASS:-}" ]] || return 1
+    set_rpc_user "$network" "$RPC_USER"
+    set_rpc_pass "$network" "$RPC_PASS"
+    return 0
+}
+
+persist_rpc_credentials() {
+    local network="$1"
+    local file
+    file="$(rpc_credentials_file "$network")"
+    cat > "$file" <<EOF
+RPC_USER=$(get_rpc_user "$network")
+RPC_PASS=$(get_rpc_password "$network")
+EOF
+    chmod 600 "$file"
+}
+
 ensure_rpc_credentials() {
     local network="$1"
     local user
@@ -33,6 +62,9 @@ ensure_rpc_credentials() {
 
 generate_rpc_credentials() {
     local network="$1"
+    if load_rpc_credentials "$network"; then
+        return
+    fi
     if [[ "${USE_EXTERNAL_BITCOIND}" == true ]]; then
         set_rpc_user "$network" "${BITCOIND_RPC_USER}"
         set_rpc_pass "$network" "${BITCOIND_RPC_PASS}"
@@ -41,6 +73,7 @@ generate_rpc_credentials() {
 
     set_rpc_user "$network" "mempool_${network}"
     set_rpc_pass "$network" "$(openssl rand -hex 32)"
+    persist_rpc_credentials "$network"
 }
 
 create_bitcoind_config() {
